0a968fddae4ae3828074d4f7e95dbfaa9d8926a3
christinaroise <54266462+christinaroise@users.noreply.github.com>
2019-12-13 06:29:56 +0100

    added build folder
    added build folder

---
+++ build/precache-manifest.a0978d443766037c64ee28959cfb1bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73e4d51d6702c6fb51d60beb3d58369",
+    "url": "/kaffemakeriet-no/index.html"
+  },
+  {
+    "revision": "bb0ea88927dcb4b20f19",
+    "url": "/kaffemakeriet-no/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7bd93dead956751d13f6",
+    "url": "/kaffemakeriet-no/static/css/main.d76ecee2.chunk.css"
+  },
+  {
+    "revision": "bb0ea88927dcb4b20f19",
+    "url": "/kaffemakeriet-no/static/js/2.77568076.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/kaffemakeriet-no/static/js/2.77568076.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bd93dead956751d13f6",
+    "url": "/kaffemakeriet-no/static/js/main.f3354717.chunk.js"
+  },
+  {
+    "revision": "2e9c31a31735da804d66",
+    "url": "/kaffemakeriet-no/static/js/runtime-main.980a7fb8.js"
+  }
+]);
